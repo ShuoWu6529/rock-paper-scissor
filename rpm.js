bf15a8ebdeaeaeb25d0005965473994e7c8f37f4
Shuo <186079699+ShuoWu6529@users.noreply.github.com>
2025-07-10 21:57:10 -0400

    Finish basic javascript

--- rpm.js
+++ rpm.js
@@ -9,56 +9,69 @@ function getComputerChoice() {
     return "scissors";
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper, or scissors?").toLowerCase();
-    if (humanChoice === "rock") {
-        return "rock";
-    } else if (humanChoice === "paper"){
-        return "paper";
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors"
+        || humanChoice === "paper" && computerChoice === "rock"
+        || humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        return 1;
+
+    } else if (humanChoice === "rock" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "rock"
+    ) {
+        return -1;
     }
-    return "scissors";
+
+    return 0;
 }
+
+function updateText(outcome, humanChoice, computerChoice){
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+
+    const result = document.querySelector(".result")
+    switch (outcome) {
+        case -1:
+            result.textContent = `You lose! ${computerChoice} beats ${humanChoice}`
+            break;
+        case 1:
+            result.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+            break;
+        case 0:
+            result.textContent = `You drew! ${humanChoice} draws ${computerChoice}`;
+            break;
+    }
+}
+
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        let loseOrWin = 2
-        if (humanChoice === "rock" && computerChoice === "scissors"
-            || humanChoice === "paper" && computerChoice === "rock"
-            || humanChoice === "scissors" && computerChoice === "paper"
-        ) {
-            loseOrWin = 1;
+    const choices = document.querySelector(".choices");
+    choices.addEventListener("click", (event) => {
+        const target = event.target;
+        const humanChoice = target.className;
+        const computerChoice = getComputerChoice()
+        let outcome = playRound(humanChoice, computerChoice);
 
-        } else if (humanChoice === "rock" && computerChoice === "paper"
-            || humanChoice === "paper" && computerChoice === "scissors"
-            || humanChoice === "scissors" && computerChoice === "rock"
-        ) {
-            loseOrWin = 0;
-        }
+        updateText(outcome, humanChoice, computerChoice);
 
-        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-
-        switch (loseOrWin) {
-            case 0:
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-                computerScore++;
-                break;
+        const human = document.querySelector(".human");
+        const computer = document.querySelector(".computer")
+        switch (outcome) {
             case 1:
-                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
                 humanScore++;
+                human.textContent = humanScore;
                 break;
-            case 2:
-                console.log(`You drew! ${humanChoice} draws ${computerChoice}`);
-                break;
+            case -1:
+                computerScore++;
+                computer.textContent = computerScore;
         }
-    }
 
-    for (let i = 0; i < 5; i++){
-        playRound(getComputerChoice(), getHumanChoice());
-    }
+        
+    })
 }
 
 playGame();
